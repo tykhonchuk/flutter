e7a5c511217e098951c8c919a065b71124f4492d
Olha Tykhonchuk <tykhonchuk.ola@gmail.com>
2025-03-24 18:49:27 +0100

    Initial commit for navigation setup

--- android/app/src/main/kotlin/com/example/git/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.git
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ navigation/android/app/src/main/kotlin/ola/tykhonchuk/com/navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package ola.tykhonchuk.com.navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
